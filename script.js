d1b53e2be6e9ef64202f07defc23b78813e6a9f5
cristian camilo gutierrez correa <gccc1108@hotmail.com>
2021-08-10 16:14:37 -0500

    nav-bar implementation

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const menuBar = document.querySelector(`.menu`);
+const secondSec = document.querySelector(`#second-section`);
+const topSec = document.querySelector(`#top-section-container`);
+const header = document.querySelector(`header`);
 
 //  Scroll into sections smoothly
 
@@ -8,3 +11,25 @@ menuBar.addEventListener("click", function (e) {
   const sect = e.target.getAttribute("href");
   document.querySelector(`${sect}`).scrollIntoView({ behavior: "smooth" });
 });
+
+//  Implementing sticky nav
+const navHeight = header.getBoundingClientRect().height;
+
+const callbackObs = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting === true)
+    document.querySelector(`header`).classList.add(`sticky`);
+  else {
+    document.querySelector(`header`).classList.remove(`sticky`);
+  }
+};
+
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const observer = new IntersectionObserver(callbackObs, options);
+observer.observe(topSec);
